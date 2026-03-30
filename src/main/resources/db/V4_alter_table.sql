a53c7473ca64ae61402d8a853ed6ec42a25e4ab0
Hai Le <giangatonglk@gmail.com>
2026-03-30 21:42:36 +0700

    alter table

--- src/main/resources/db/V4_alter_table.sql
+++ src/main/resources/db/V4_alter_table.sql
@@ -1,13 +1,23 @@
 ALTER TABLE inventory ADD column if not exists process_date date;
 
-ALTER TABLE formula ADD column if not exists formula_id UUID;
-ALTER TABLE formula ADD column if not exists formula_version INTEGER;
+ALTER TABLE product_prices ADD column if not exists formula_id UUID;
+ALTER TABLE product_prices ADD column if not exists formula_version INTEGER;
 
--- FK product
-ALTER TABLE formula
-ADD CONSTRAINT fk_formula_product
-FOREIGN KEY (product_code)
-REFERENCES product(code);
+ALTER TABLE formula RENAME COLUMN formula_version TO formula_versions;
+
+
+
+CREATE TABLE if not exists unit_conversion (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v7(),
+    from_unit_code varchar(20) REFERENCES unit(code),
+    to_unit_code varchar(20) REFERENCES unit(code),
+    ratio  NUMERIC(18,6),
+    status varchar(10),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_by VARCHAR(100) DEFAULT 'SYSTEM',
+    updated_by VARCHAR(100),
+    updated_at TIMESTAMP
+);
 
 -- chỉ 1 formula active / product
 CREATE UNIQUE INDEX uk_formula_active
