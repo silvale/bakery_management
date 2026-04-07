9ed7eb9f4deb1b1d47e5bacf53c34ae2b44807c0
Hai Le <giangatonglk@gmail.com>
2026-04-08 00:09:38 +0700

    update sql script

--- src/main/resources/db/V5_alter_table.sql
+++ src/main/resources/db/V5_alter_table.sql
@@ -2,14 +2,8 @@
 ALTER TABLE product
 ADD COLUMN if not EXISTS supplier_code VARCHAR(50);
 
-CREATE table if not EXISTS product_inventory_config (
-    product_code VARCHAR(50),
-    warehouse_type VARCHAR(50),
-    warning_quantity NUMERIC,
-    created_at timestamptz(6) DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	created_by varchar(255) DEFAULT 'SYSTEM' NOT NULL,
-    PRIMARY KEY (product_code, warehouse_type)
-);
+ALTER TABLE product
+ADD COLUMN if not EXISTS warning_quantity int;
 
 
 CREATE table if not EXISTS supplier (
